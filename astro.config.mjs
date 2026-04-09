a40961a9812336dcbd9700437a26c94368622165
Danny Hoang <hoangvthu2109@gmail.com>
2026-04-08 20:39:27 -0700

    feat: integrate network blueprint advisor to portfolio

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,5 @@
+// @ts-check
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({
-  // Your main portfolio address
-  site: 'https://dannyhng.github.io',
-  // The specific folder name for this tool
-  base: '/network-advisor',
-});
+// https://astro.build/config
+export default defineConfig({});
